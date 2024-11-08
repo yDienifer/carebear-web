9b5c27d87f24771680bc7970e9b7a377fd32f199
Jenny <yuri@archlinux.localdomain>
2024-11-08 19:06:26 -0300

    Ajusta configuração do ESLint para o formato flat, removendo chaves incompatíveis como 'env' e 'extends', e adiciona 'type': 'module' no package.json

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import react from 'eslint-plugin-react';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+      },
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      react,
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    rules: {
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+
+    ignores: ['.next/', 'node_modules/', 'build/'],
+  },
+];
